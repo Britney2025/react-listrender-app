748713b475e553ca18e77b3c972b44ce8770ca13
Britney2025 <chosenbright1@gmail.com>
2025-07-16 15:45:50 +0300

    feat: Add the delete book feature

--- src/Components/BookItem.jsx
+++ src/Components/BookItem.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function BookItem() {
-  return (
-    <div>BookItem</div>
-  )
+function BookItem({ book, onDelete }) {
+    return (
+        <div>
+            <h3>{book.title}</h3>
+            <p>By: {book.author}</p>
+            <button
+                onClick={() => onDelete(book.id)}
+            >
+                Delete
+            </button>
+        </div>
+    )
 }
 
 export default BookItem
