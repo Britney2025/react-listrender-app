512f3b2e206a54bf004e068881d7e820b05a7631
Britney2025 <chosenbright1@gmail.com>
2025-07-16 15:14:42 +0300

    Add form input state for title and author

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-
+import React, { useState } from 'react'
+import Booklist from './Components/Booklist'
 
 function App() {
-    return (
-    <h1 class="font-bold underline text-3xl">Hello World</h1>
+  const [books, setBooks] = useState([
+    { id: 101, title: "Atomic Habits", author: "James Clear" },
+    { id: 102, title: "The Alchemist", author: "Paulo Coelho" }
+  ]);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleAddBook = () => {
+    if (title && author) {
+      const newBook = {
+        id: Date.now(),
+        title,
+        author
+      };
+      setBooks([...books, newBook]);
+      setTitle('');
+      setAuthor('');
+    }
+  };
+
+  const handleDelete = (id) => {
+    setBooks(books(book = book.id !== id))
+  };
+  return (
+    <div>
+      <div>
+        <h1>Book List</h1>
+        <div>
+          <input 
+          type="text" 
+          placeholder='Book Title'
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          />
+          <input 
+          type="text" 
+          placeholder='Author'
+          value={author}
+          onChange={e => setAuthor(e.target.value)}
+          />
+          <button 
+          onClick={handleAddBook}
+          >
+            Add
+          </button>
+        </div>
+        <Booklist books={ books } onDelete={handleDelete}/>
+      </div>
+    </div>
   )
 }
 
