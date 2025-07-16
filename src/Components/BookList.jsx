43bb1f023825c15a66036587896f2995e0da00c7
Britney2025 <chosenbright1@gmail.com>
2025-07-16 15:38:06 +0300

    Render book lists using .map

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import BookItem from './BookItem'
 
-function Booklist() {
-  return (
-    <div>Booklist</div>
-  )
+function Booklist({ books, onDelete }) {
+    return (
+        <div>
+            {books.length === 0 ? (
+                <p>No books in this list.</p>
+            ) : (
+                books.map(book => (
+                    <BookItem key={book.id} book={book} onDelete={onDelete}/>
+                ))
+            )}
+        </div>
+    )
 }
 
 export default Booklist
